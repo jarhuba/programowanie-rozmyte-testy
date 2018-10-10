5aa8c8b397b094d8d98b588a1e1d9daa4c20c2af
jarhuba <jarhuba@gmail.com>
2018-10-10 11:08:06 +0200

    First commit, empty project

---
+++ src/main/java/com/fuzzy/fuzzysets/FuzzySetsApplication.java
@@ -0,0 +1,12 @@
+package com.fuzzy.fuzzysets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzySetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuzzySetsApplication.class, args);
+    }
+}
